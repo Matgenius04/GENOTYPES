4aa2ebdd806874b17e1fd210af452d3bdb52b0d6
F4Tornado <h.rovnyak@gmail.com>
2018-01-29 17:40:20 -0500

    Drawing thingies

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ let momCombos = [];
 function setup() {
   createCanvas(windowWidth, windowHeight).position(0, 0).parent("#can");
   select("#geno-dad").position(windowWidth * 0.05, windowHeight * 0.05);
-  select("#geno-mom").position(windowWidth * 0.05, windowHeight * 0.07);
+  select("#geno-mom").position(windowWidth * 0.05, windowHeight * 0.09);
 }
 
 function draw() {
@@ -15,17 +15,22 @@ function draw() {
   background(235);
   fill(51);
   if (dad.length % 2 === 0 && dad.length === mom.length) {
-    if (dadCombos.length < dad.length) {
+    if (dadCombos.length != dad.length) {
       dadCombos = [];
       momCombos = [];
-      for (let i = 0; i < pow(2, dad.length/2); i++) {
+      for (let i = 0; i < pow(2, dad.length / 2); i++) {
         dadCombos.push(combo(dad, i));
-        momCombos.push(combo(mom, i))
+        momCombos.push(combo(mom, i));
       }
     }
     for (let i = 0; i < pow(2, dad.length / 2) + 1; i++) {
       line(width * 0.25, height * (i * 0.0625 + 0.25), width * (pow(2, mom.length / 2) * 0.0625 + 0.25), height * (i * 0.0625 + 0.25));
       line(width * (i * 0.0625 + 0.25), height * 0.25, width * (i * 0.0625 + 0.25), height * (pow(2, mom.length / 2) * 0.0625 + 0.25));
+      if (dadCombos.length != 0 && momCombos.length != 0 && i < pow(2, dad.length / 2)) {
+        textSize(width / 80);
+        text(momCombos[i], width * 0.24 - textWidth(momCombos[i]), height * ((i+0.5) * 0.0625 + 0.25));
+        text(dadCombos[i], width * ((i+0.2) * 0.0625 + 0.25), height * 0.24);
+      }
     }
   }
 }
@@ -34,19 +39,18 @@ function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
   select("canvas").position(0, 0);
   select("#geno-dad").position(windowWidth * 0.05, windowHeight * 0.05);
-  select("#geno-mom").position(windowWidth * 0.05, windowHeight * 0.07);
+  select("#geno-mom").position(windowWidth * 0.05, windowHeight * 0.09);
 }
 
 function combo(geno, ie) {
   let i = ie.toString(2).split("");
-  for (j = 0; j < geno.length/2; j++) {
+  for (j = 0; j < geno.length / 2; j++) {
     if (i[j] == undefined) {
       i.unshift(0);
     } else {
       i[j] = parseInt(i[j], 10);
     }
   }
-  console.log(i);
   let ret = "";
   for (let j = 0; j < geno.length / 2; j++) {
     ret += geno[(j * 2) + i[j]];
